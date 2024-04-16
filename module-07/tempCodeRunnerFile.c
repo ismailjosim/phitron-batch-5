68d7b3bf6a2e69a9dd8d27543d493111ce9a8d64
ismailjosim <ismailjosim99@gmail.com>
2024-04-17 01:14:45 +0600

    module 07 initial project

---
+++ module-07/tempCodeRunnerFile.c
@@ -0,0 +1,15 @@
+    // int x[] = {1, 2, 3, 4, 5};
+    // int y[5] = {100};
+
+    // // array declaration
+    // int arr[10];
+    // int arrLen = sizeof(arr) / sizeof(arr[0]); // 40 / 4
+
+    // for (int i = 0; i < arrLen; i++)
+    // {
+    //     // array input
+    //     scanf("%d", &arr[i]);
+
+    //     // array Output
+    //     // printf("%d\n", arr[i]);
+    // }
