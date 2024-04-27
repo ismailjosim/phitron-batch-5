ced9b3a694b7b750280efa98c58a423298ac1299
ismailjosim <ismailjosim99@gmail.com>
2024-04-27 23:00:46 +0600

    pattern 01: completed

--- module-13 nested loop recap/pattern-01.c
+++ module-13 nested loop recap/pattern-01.c
@@ -2,6 +2,23 @@
 
 int main()
 {
+    // step-01: declare variables and take input
+    int n, k = 1;
+    scanf("%d", &n);
+
+    // step-02: run a loop and print "work" for "n" times
+    for (int i = 0; i < n; i++)
+    {
+        // step-03: Define works
+        for (int x = 0; x < k; x++)
+        {
+            printf("* ");
+        }
+
+        // step-04: add new ine after each work
+        printf("\n");
+        k++;
+    }
 
     return 0;
 }
