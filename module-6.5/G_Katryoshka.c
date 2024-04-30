f93aa5a7c040d555840941bd08bd9be2890af464
ismailjosim <ismailjosim99@gmail.com>
2024-05-01 00:40:54 +0600

    add All Files

--- module-6.5/G_Katryoshka.c
+++ module-6.5/G_Katryoshka.c
@@ -2,10 +2,36 @@
 
 int main()
 {
-    int x, y, z;
-    scanf("%d %d %d", &x, &y, &z);
+    int x, y;
+    long long int z;
+    scanf("%d %d %lld", &x, &y, &z);
 
-    //    find the total number of
+    int a = 0;
+    if (x < y && x < z)
+    {
+        a = x;
+        y -= x;
+        z -= x;
+    }
+    else if (y < x && y < z)
+    {
+        a = y;
+        x -= y;
+        z -= y;
+    }
+    else
+    {
+        a = z;
+        x -= z;
+        y -= z;
+    }
+
+    if (x > z)
+    {
+        a += x / 2;
+    }
+
+    printf("%d", a);
 
     return 0;
 }
