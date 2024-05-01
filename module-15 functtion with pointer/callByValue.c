34ad0be7f54ac79795269516eacc5f44d182b0cc
ismailjosim <ismailjosim99@gmail.com>
2024-05-02 00:15:11 +0600

    Function: Call by Value

--- module-15 functtion with pointer/callByValue.c
+++ module-15 functtion with pointer/callByValue.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+void add(int value)
+{
+    value = 20;
+    printf("Modified value: %d\n", value);
+    printf("Modified Memory Address: %p\n", value);
+}
 
 int main()
 {
-
+    int originalValue = 10;
+    add(originalValue);
+    printf("Original value: %d\n", originalValue);
+    printf("Original  Memory Address: %p\n", originalValue);
     return 0;
 }
