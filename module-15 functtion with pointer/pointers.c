31385d75c191b0d33dc728eb97364c95b7e285f7
ismailjosim <ismailjosim99@gmail.com>
2024-05-02 00:05:46 +0600

    module 15:pointers with different data types

--- module-15 functtion with pointer/pointers.c
+++ module-15 functtion with pointer/pointers.c
@@ -3,19 +3,30 @@
 int main()
 {
     int x = 10;
-    printf("%p\n", &x);
+    // printf("%p\n", &x);
 
     // pointers
     int *p = &x;
-    printf("%p\n", p);
+    // printf("%p\n", p);
 
     //* dereference : retrieve value from memory address(pointers)
-    printf("%d\n", *p);
+    // printf("%d\n", *p);
 
     //* Change value using memory address(pointers)
     *p = 500;
-    printf("%d\n", x);
-    printf("%p\n", *p);
+    // printf("%d\n", x);
+    // printf("%p\n", *p);
+
+    // with double data-types
+    double y = 5.55;
+    double *ptr = &y;
+
+    printf("%0.2lf\n", y);
+    printf("%0.2lf\n", *ptr);
+    printf("Y memory reference: %p\n", ptr);
+    *ptr = 10.20;
+    printf("%0.2lf\n", *ptr);
+    printf("Y memory reference: %p\n", ptr);
 
     return 0;
 }
