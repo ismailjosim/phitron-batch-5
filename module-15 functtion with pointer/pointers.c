9b5db53ce5aceaa6e2218318b0f378aa26498700
ismailjosim <ismailjosim99@gmail.com>
2024-05-01 16:18:04 +0600

    module 14:pointers

--- module-15 functtion with pointer/pointers.c
+++ module-15 functtion with pointer/pointers.c
@@ -2,6 +2,20 @@
 
 int main()
 {
+    int x = 10;
+    printf("%p\n", &x);
+
+    // pointers
+    int *p = &x;
+    printf("%p\n", p);
+
+    //* dereference : retrieve value from memory address(pointers)
+    printf("%d\n", *p);
+
+    //* Change value using memory address(pointers)
+    *p = 500;
+    printf("%d\n", x);
+    printf("%p\n", *p);
 
     return 0;
 }
---
+++ module-15 functtion with pointer/tempCodeRunnerFile.c
@@ -0,0 +1,3 @@
+*p = 500;
+    // printf("%d\n", x);
+    // printf("%p\n", *p);
