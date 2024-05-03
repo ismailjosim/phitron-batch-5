ab512c704708eaef50d3750b4730ad04e70a0990
ismailjosim <ismailjosim99@gmail.com>
2024-05-03 11:18:55 +0600

    function with array

--- module-15 functtion with pointer/function_and_array.c
+++ module-15 functtion with pointer/function_and_array.c
@@ -1,7 +1,43 @@
 #include <stdio.h>
 
+void add(int ar[], int n) // with array int value
+void add(int *ar, int n) // with pointers
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", ar[i]);
+    }
+}
+
 int main()
 {
+    int ar[5] = {10, 20, 30, 40, 50};
+    int n = sizeof(ar) / sizeof(int);
+    add(ar, n);
 
+    return 0;
+}
+
+#include <stdio.h>
+
+void sum(double *arr, int n)
+{
+    double total = 0;
+    for (int i = 0; i < n; i++)
+    {
+        total += arr[i];
+    }
+    printf("%0.21f\n", total);
+}
+
+int main()
+{
+    double arr[5] = {10.4,
+                     15.99,
+                     20.20,
+                     14.30,
+                     50.5};
+    int n = sizeof(arr) / sizeof(int);
+    sum(arr, n);
     return 0;
 }
