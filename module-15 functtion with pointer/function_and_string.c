602f5f20fba1ae98f4ec553581d2bab9ba22db1c
ismailjosim <ismailjosim99@gmail.com>
2024-05-03 12:57:13 +0600

    module-15: practice completed

--- module-15 functtion with pointer/function_and_string.c
+++ module-15 functtion with pointer/function_and_string.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
+#include <string.h>
+
+void add(char arr[])
+
+{
+    printf("%d", strlen(arr)); // 5
+
+    // ? we don't need length in string.
+}
 
 int main()
 {
+    char arr[6] = "Hello";
+
+    add(arr);
 
     return 0;
 }
