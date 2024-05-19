036a47f914865372c0407a2f7adcebd2ce86dc3e
ismailjosim <ismailjosim99@gmail.com>
2024-05-19 19:14:10 +0600

    switch cases in c++

--- CPP-for-DSA/module-01 Basic CPP/if-else-ternary-operator.cpp
+++ CPP-for-DSA/module-01 Basic CPP/if-else-ternary-operator.cpp
@@ -4,6 +4,14 @@ using namespace std;
 
 int main()
 {
+    // * in c and c++ if-else condition 100% same
+
+    // * ternary operator is the shorter version of if-else condition
+
+    int x = 100;
+    int y = 20;
+
+    (x > y) ? cout << "X Is Greater then Y" : cout << "Y is Greater then X";
 
     return 0;
 }
