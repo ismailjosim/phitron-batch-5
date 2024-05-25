b1af88ac0746d8064847531060e232630071cea9
ismailjosim <ismailjosim99@gmail.com>
2024-05-25 22:45:52 +0600

    string built-in method: capacity

---
+++ CPP-for-DSA/module-05 string class in C++/capacity.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    //* s.size() -> returns the size of the string.
+    // cout << s.size() << endl; // ans: 6
+
+    //* s.max_size() -> returns the maximum size that string can hold.
+    // cout << s.max_size() << endl; // 1073741823 it may very depending of machine
+
+    //* s.capacity() -> returns current available capacity of the string.
+    // cout << s.capacity(); // how much can it hold
+
+    //* s.clear() -> clear the string.
+    // cout << s << endl;
+    // s.clear();
+    // cout << s << endl;
+
+    //* s.empty() -> return true/false if the string is empty.
+    // if (s.empty())
+    // {
+    //     cout << "empty" << endl;
+    // }
+    // else
+    // {
+    //     cout << "Not Empty" << endl;
+    // }
+
+    //* s.resize() -> change the size of the string.
+
+    return 0;
+}
