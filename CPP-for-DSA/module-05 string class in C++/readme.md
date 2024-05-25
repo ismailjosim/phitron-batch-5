bc407ef4b242ff394ed33c7a407c1f48776975e4
ismailjosim <ismailjosim99@gmail.com>
2024-05-25 22:26:22 +0600

    what is string

---
+++ CPP-for-DSA/module-05 string class in C++/readme.md
@@ -0,0 +1,7 @@
+# Topics
+
+- String In C++ ✅
+- built in function in C++
+- input string with space
+- output word by word
+- know about Iterator
---
+++ CPP-for-DSA/module-05 string class in C++/string.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string p1, p2;
+    cin >> p1 >> p2;
+    if (p1 == p2)
+    {
+        cout << "Same" << endl;
+    }
+    else
+    {
+        cout << "Not Same" << endl;
+    }
+
+    return 0;
+}
