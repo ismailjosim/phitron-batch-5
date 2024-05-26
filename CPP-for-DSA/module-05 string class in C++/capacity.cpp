c8d247592434fa6d99eb44d68564baecf21446de
ismailjosim <ismailjosim99@gmail.com>
2024-05-26 16:31:33 +0600

    orgainize code

--- CPP-for-DSA/module-05 string class in C++/capacity.cpp
+++ CPP-for-DSA/module-05 string class in C++/capacity.cpp
@@ -7,26 +7,26 @@ int main()
     //* s.size() -> returns the size of the string.
     string s;
     cin >> s;
-    // cout << s.size() << endl;
+    cout << s.size() << endl;
     //* s.max_size() -> returns the maximum size that string can hold.
-    // cout << s.max_size() << endl; // 1073741823
+    cout << s.max_size() << endl; // 1073741823
     //* s.capacity() -> returns current available capacity of the string. will increase if string size if larger the default 15 size
-    // cout << s.capacity() << endl;
+    cout << s.capacity() << endl;
 
     //* s.clear() -> clear the string.
-    // cout << s << endl;
-    // s.clear();
-    // cout << s << endl;
+    cout << s << endl;
+    s.clear();
+    cout << s << endl;
 
     // s.empty() -> return true/false if the string is empty.
-    // if (!s.empty())
-    // {
-    //     cout << "True" << endl;
-    // }
-    // else
-    // {
-    //     cout << "False" << endl;
-    // }
+    if (!s.empty())
+    {
+        cout << "True" << endl;
+    }
+    else
+    {
+        cout << "False" << endl;
+    }
 
     // s.resize() -> change the size of the string.
     string str = "Javascript";
