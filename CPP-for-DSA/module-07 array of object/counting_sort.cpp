2dc34dd8110967fae728790eef003c1a63c366b2
ismailjosim <ismailjosim99@gmail.com>
2024-05-30 09:58:13 +0600

    counting sort for sort string

--- CPP-for-DSA/module-07 array of object/counting_sort.cpp
+++ CPP-for-DSA/module-07 array of object/counting_sort.cpp
@@ -4,6 +4,31 @@ using namespace std;
 
 int main()
 {
+    string s;
+    getline(cin, s);
+    int frq[26] = {};
+    for (char c : s)
+    {
+        frq[c - 'a']++;
+    }
+
+    for (char i = 'a'; i <= 'z'; i++)
+    {
+        //* print only the value that is greater then 0
+        if (frq[i - 'a'] != 0)
+        {
+            cout << i << " - " << frq[i - 'a'] << endl;
+        }
+    }
+    for (char i = 'a'; i <= 'z'; i++)
+    {
+
+        //* sort the string
+        for (int j = 0; j < frq[i - 'a']; j++)
+        {
+            cout << i;
+        }
+    }
 
     return 0;
 }
