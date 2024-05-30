2dadf19f5bd41ead789236244148a1137728c454
ismailjosim <ismailjosim99@gmail.com>
2024-05-30 08:50:42 +0600

    selection sort based on marks and age if maks is same

--- CPP-for-DSA/module-07 array of object/sort_selection.cpp
+++ CPP-for-DSA/module-07 array of object/sort_selection.cpp
@@ -1,9 +1,64 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
+class Student
+{
+public:
+    string name;
+    int age;
+    int marks;
+};
 int main()
 {
+    int n;
+    cin >> n;
+
+    Student arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+
+        cin >> arr[i].name >> arr[i].age >> arr[i].marks;
+    };
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i].marks > arr[j].marks)
+            {
+                swap(arr[i], arr[j]);
+            }
+        }
+    }
+    for (int i = 0; i < n - 1; i++)
+    {
+        cout << arr[i].name << " " << arr[i].age << " " << arr[i].marks << endl;
+    }
+
+    // if 2 value has same marks then sort will be based on age number (less number)
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i].marks > arr[j].marks)
+            {
+                swap(arr[i], arr[j]);
+            }
+            if (arr[i].marks == arr[j].marks)
+            {
+                if (arr[i].age > arr[j].age)
+                {
+                    swap(arr[i], arr[j]);
+                }
+            }
+        }
+    }
+    cout << endl;
+    for (int i = 0; i < n - 1; i++)
+    {
+        cout << arr[i].name << " " << arr[i].age << " " << arr[i].marks << endl;
+    }
 
     return 0;
 }
