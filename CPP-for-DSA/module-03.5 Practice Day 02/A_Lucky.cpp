af9a93e5f64f79ac41cb8a6fc8ca78a52b60c5cf
ismailjosim <ismailjosim99@gmail.com>
2024-06-15 23:44:11 +0600

    module 03: starter files

---
+++ CPP-for-DSA/module-03.5 Practice Day 02/A_Lucky.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    int left = 0;
+    int right = 0;
+    for (int i = 0; i < 3; i++)
+    {
+    }
+    for (int i = 0; i < 3; i++)
+    {
+    }
+
+    return 0;
+}
