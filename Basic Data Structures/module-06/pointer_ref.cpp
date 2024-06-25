a579d7016b269492ff480e9c0f57cc93879d4e16
ismailjosim <ismailjosim99@gmail.com>
2024-06-25 17:42:56 +0600

    insert at tail

---
+++ Basic Data Structures/module-06/pointer_ref.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+void fun(int *p)
+{
+    // p = NULL;
+    cout << &p << endl;
+};
+
+int main()
+{
+    int v = 10;
+    int *ptr = &v;
+    fun(ptr);
+
+    // cout << v << endl;
+    cout << &ptr << endl; // extract memory address
+
+    return 0;
+}
