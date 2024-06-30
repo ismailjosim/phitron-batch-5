9133cfa9b6bf0301ed66a732532f02f50e7e551d
ismailjosim <ismailjosim99@gmail.com>
2024-06-30 22:24:43 +0600

    doubly linked list insert head, any pos and tail

--- Basic Data Structures/module-09/insert_at_any_pos.cpp
+++ Basic Data Structures/module-09/insert_at_any_pos.cpp
@@ -27,6 +27,7 @@ void print_normal(Node *head)
     }
     cout << endl;
 }
+
 void print_reverse(Node *tail)
 {
     Node *tmp = tail;
@@ -39,30 +40,46 @@ void print_reverse(Node *tail)
 }
 
 // insert at any position
-void insert_pos(Node *head, int pos, int v)
+void insert_pos(Node *&head, int pos, int v)
 {
     Node *newNode = new Node(v);
+
+    // if inserting at the head (pos == 0)
+    if (pos == 0)
+    {
+        newNode->next = head;
+        if (head != NULL)
+            head->prev = newNode;
+        head = newNode;
+        return;
+    }
 
     // step 01: create temp head
     Node *temp = head;
 
     // step 02: loop through that specific position
-    for (int i = 0; i < pos; i++)
+    for (int i = 0; i < pos - 1; i++)
     {
+        if (temp->next == NULL)
+        {
+            cout << "Position out of bounds" << endl;
+            return;
+        }
         temp = temp->next;
     }
 
-    // step 03: connect temp node with newNode->next
-    newNode->next = temp; // 100->30
+    // step 03: connect newNode->next to temp->next
+    newNode->next = temp->next;
 
-    // step 04: connect newNode with Temp node nex
-    temp->next = newNode; // 20->100
+    // step 04: connect temp node's next to newNode
+    temp->next = newNode;
 
-    // step 05: connect next of the newNode Prev with newNode
-    newNode->next->prev = newNode; // 100<-30
-
-    // step 05: connect temp with newNode Prev
+    // step 05: connect newNode->prev to temp
     newNode->prev = temp;
+
+    // step 06: if newNode->next is not NULL, connect its prev to newNode
+    if (newNode->next != NULL)
+        newNode->next->prev = newNode;
 }
 
 int main()
@@ -72,6 +89,16 @@ int main()
     Node *b = new Node(30);
     Node *c = new Node(40);
     Node *tail = new Node(50);
+
+    // Manually connect the nodes
+    head->next = a;
+    a->prev = head;
+    a->next = b;
+    b->prev = a;
+    b->next = c;
+    c->prev = b;
+    c->next = tail;
+    tail->prev = c;
 
     insert_pos(head, 2, 100);
     print_normal(head);
