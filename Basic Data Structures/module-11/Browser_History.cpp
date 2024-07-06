2f85dcab688df0498f5253f4f5ac05372b819569
ismailjosim <ismailjosim99@gmail.com>
2024-07-06 13:54:37 +0600

    solve remove duplicate again, Palindrom and remember previous queries problem

---
+++ Basic Data Structures/module-11/Browser_History.cpp
@@ -0,0 +1,10 @@
+// amader list er modde je item gula ase oigula jodi visit dewa thake ar set amader doubly linked list er modde exist kore tahole oita print kore dite hobe. er pore ase prev mane tar ger ta jodi list e thake tahole print next e jodi list e thake print
+
+/*
+* facebook, google, Phitron, youtube twitter
+* visit Phitron == Phitron
+* visit prev => google
+* visit prev => facebook
+* visit prev => NULL so (Not Available)
+
+*/
---
+++ Basic Data Structures/module-11/Remember_Previous_Queries.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void print_forward(list<int> lists)
+{
+    // print out
+    cout << "L -> ";
+    for (auto it = lists.begin(); it != lists.end(); it++)
+    {
+        cout << *it << " ";
+    }
+    cout << endl;
+}
+// void print_backward(list<int> lists)
+// {
+//     // print out
+//     cout << "R -> ";
+//     for (auto it = lists.rbegin(); it != lists.rend(); it++)
+//     {
+//         cout << *it << " ";
+//     }
+//     cout << endl;
+// }
+
+void print_backward(list<int> lists)
+{
+    // Print out
+    cout << "R -> ";
+    auto it = lists.end();
+    while (it != lists.begin())
+    {
+        it--;
+        cout << *it << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    list<int> lists;
+    int t;
+    cin >> t;
+    while (t > 0)
+    {
+        int p, v;
+        cin >> p >> v;
+        if (p == 0)
+        {
+            lists.push_front(v);
+        }
+        else if (p == 1)
+        {
+            lists.push_back(v);
+        }
+        else if (p == 2)
+        {
+            if (v >= 0 && v < lists.size())
+            {
+                auto it = next(lists.begin(), v);
+                lists.erase(it);
+            }
+        }
+        print_forward(lists);
+        print_backward(lists);
+        t--;
+    }
+
+    return 0;
+}
