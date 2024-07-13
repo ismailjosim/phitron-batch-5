84b31540828b890a3ef50ffe1d0b719fe578d5fe
ismailjosim <ismailjosim99@gmail.com>
2024-07-13 12:46:49 +0600

    solve time nned to buy ticket problem

--- Basic Data Structures/conceptual-session-04/time-needed-to-buy-ticket.cpp
+++ Basic Data Structures/conceptual-session-04/time-needed-to-buy-ticket.cpp
@@ -16,5 +16,40 @@ int main()
             tickets.push_back(v);
     }
 
+    int k;
+    cin >> k;
+
+    int time = 0;
+    queue<pair<int, int>> q;
+
+    for (int i = 0; i < tickets.size(); i++)
+    {
+        q.push({tickets[i], i});
+    }
+
+    while (!q.empty())
+    {
+        auto current = q.front();
+        q.pop();
+
+        if (current.first > 0)
+        {
+            current.first--;
+            time++;
+
+            if (current.first > 0)
+            {
+                q.push(current);
+            }
+
+            if (current.second == k && current.first == 0)
+            {
+                break;
+            }
+        }
+    }
+
+    cout << time << endl;
+
     return 0;
 }
