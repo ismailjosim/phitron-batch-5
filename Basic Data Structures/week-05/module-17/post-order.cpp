9f2e9870b36d9ef74abdaac386a160caa9d5900f
ismailjosim <ismailjosim99@gmail.com>
2024-09-04 17:39:42 +0600

    binary tree: pre-order, post-order and in-order print

---
+++ Basic Data Structures/week-05/module-17/post-order.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    // constructor
+    Node(int data)
+    {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+// POST Order print function
+void inOrder(Node *root)
+{
+    // step 04: base case:
+    if (root == NULL)
+    {
+        return;
+    };
+
+    // step 01: print left
+    inOrder(root->left);
+
+    // step 02: print the value
+    cout << root->data << " ";
+
+    // step 03: print right
+    inOrder(root->right);
+}
+
+int main()
+{
+    //* Create NODE
+    Node *root = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *c = new Node(40);
+    Node *d = new Node(50);
+    Node *e = new Node(60);
+    Node *f = new Node(70);
+    Node *g = new Node(80);
+    Node *h = new Node(90);
+    Node *i = new Node(100);
+
+    //* connection
+    root->left = a;
+    root->right = b;
+    a->left = c;
+    a->right = h;
+    c->right = e;
+    b->right = d;
+    d->left = f;
+    d->right = g;
+    h->right = i;
+
+    inOrder(root);
+
+    return 0;
+}
