3e3344f14a0f2771d10f6eea2c40438d8a350f2a
ismailjosim <ismailjosim99@gmail.com>
2024-09-04 17:49:41 +0600

    Operations on binary tree

---
+++ Basic Data Structures/week-05/module-18/level-order.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    // constructor
+    Node(int data)
+    {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
