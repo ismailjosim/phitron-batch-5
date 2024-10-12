3585bb84b52465e8f97a79bf881c62b1b86d5709
ismailjosim <ismailjosim99@gmail.com>
2024-10-12 15:34:32 +0600

    adjacency matrix in practice with proper comment

--- Introduction to Algorithms/week-01/module-01/adjacency_matrix.cpp
+++ Introduction to Algorithms/week-01/module-01/adjacency_matrix.cpp
@@ -5,34 +5,36 @@ using namespace std;
 int main()
 {
     int n, e;
-    cin >> n >> e;
+    cin >> n >> e; // Input number of nodes (n) and edges (e)
 
-    int mat[n][n];
-    memset(mat, 0, sizeof(mat));
+    int mat[n][n];               // Initialize adjacency matrix for the graph
+    memset(mat, 0, sizeof(mat)); // Set all elements of the matrix to 0 (no edges initially)
 
+    // Input edges and mark them in the adjacency matrix
     while (e--)
     {
         int a, b;
-        cin >> a >> b;
-        mat[a][b] = 1;
-        mat[b][a] = 1; //* directed graph ei part ta add hobe na.
+        cin >> a >> b; // Input the two endpoints of an edge
+        mat[a][b] = 1; // Mark edge from node a to node b
+        mat[b][a] = 1; // Mark edge from node b to node a (only for undirected graphs)
+                       // * For directed graphs, this line should be omitted
     }
 
-    //* Print value
+    // Print the adjacency matrix
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            cout << mat[i][j] << " ";
+            cout << mat[i][j] << " "; // Output 0 or 1 for each element in the matrix
         }
-        cout << endl;
+        cout << endl; // Move to the next row after each line
     }
 
-    //* check connection
+    // Check if node 1 and node 4 are connected
     if (mat[1][4] == 1)
-        cout << "Connected" << endl;
+        cout << "Connected" << endl; // Print "Connected" if there is an edge between node 1 and 4
     else
-        cout << "Not Connected" << endl;
+        cout << "Not Connected" << endl; // Print "Not Connected" if there is no edge
 
     return 0;
 }
