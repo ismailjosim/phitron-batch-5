684e086348ee2452090b6d164be845f40010d08e
ismailjosim <ismailjosim99@gmail.com>
2024-10-12 22:31:55 +0600

    1-6 Adjacency List Implementation

--- Introduction to Algorithms/week-01/module-01/adjacency_list.cpp
+++ Introduction to Algorithms/week-01/module-01/adjacency_list.cpp
@@ -4,6 +4,31 @@ using namespace std;
 
 int main()
 {
+    int n, e;
+    cin >> n >> e;
+
+    vector<int> mat[n];
+
+    while (e--)
+    {
+        int a, b;
+        cin >> a >> b;
+
+        mat[a].push_back(b);
+        mat[b].push_back(a);
+    }
+
+    // * print array 3rd index vector value
+    // for (int i = 0; i < mat[3].size(); i++)
+    // {
+    //     cout << mat[3][i] << " ";
+    // }
+
+    // * short method
+    for (int x : mat[0])
+    {
+        cout << x << " ";
+    }
 
     return 0;
 }
