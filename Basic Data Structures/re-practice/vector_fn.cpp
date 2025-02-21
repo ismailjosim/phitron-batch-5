2a68bf48869d45ab35cb32c8d5c045675ff27642
ismailjosim <ismailjosim99@gmail.com>
2025-02-21 09:19:17 +0600

    module 2 revision done✅

---
+++ Basic Data Structures/re-practice/vector_fn.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    // first type of vector declaration
+    vector<int> v;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        v.push_back(x);
+    }
+
+    // second type of vector declaration
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+
+    return 0;
+}
