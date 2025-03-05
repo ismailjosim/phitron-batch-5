b2675b55be236f024ceeebef932a7303c1b11a6c
ismailjosim <ismailjosim99@gmail.com>
2025-03-05 10:53:50 +0600

    solve the problem using function

--- Basic Data Structures/week-01/module-3.5/running_sum_array.cpp
+++ Basic Data Structures/week-01/module-3.5/running_sum_array.cpp
@@ -2,6 +2,18 @@
 
 using namespace std;
 
+vector<int> runningSum(vector<int> &arr)
+{
+    int n = arr.size();
+    vector<int> running_sum(n);
+    running_sum[0] = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        running_sum[i] = running_sum[i - 1] + arr[i];
+    }
+    return running_sum;
+}
+
 int main()
 {
     int n;
@@ -11,13 +23,7 @@ int main()
     {
         cin >> arr[i];
     };
-    vector<int> running_sum(n);
-    running_sum[0] = arr[0];
-    for (int i = 1; i < n; i++)
-    {
-        running_sum[i] = running_sum[i - 1] + arr[i];
-    }
-
+    vector<int> running_sum = runningSum(arr);
     for (int i = 0; i < n; i++)
     {
         cout << running_sum[i] << " ";
